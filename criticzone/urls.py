de5572009d8e4453e89d990bb829c3d8cf5fca2e
Anshul <anshuldhakar03@gmail.com>
2022-05-16 10:51:26 +0530

    made the favicon.ico work

--- criticzone/urls.py
+++ criticzone/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
+     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('media/favicon.ico'))),
     path('', views.index, name="index"),
 ]
